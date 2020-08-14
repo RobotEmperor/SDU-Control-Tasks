b55679184e276c055a0be4d01a13751ce0a0f203
robotemperor <yik@mmmi.sdu.dk>
2020-08-14 11:22:51 +0200

    first uplaod

---
+++ belt_task/build/CMakeFiles/belt_task.dir/cmake_clean.cmake
@@ -0,0 +1,14 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/belt_task.dir/src/data_logging.cpp.o"
+  "CMakeFiles/belt_task.dir/src/task_motion.cpp.o"
+  "CMakeFiles/belt_task.dir/src/ros_node.cpp.o"
+  "CMakeFiles/belt_task.dir/src/main.cpp.o"
+  "CMakeFiles/belt_task.dir/src/tool_estimation.cpp.o"
+  "belt_task.pdb"
+  "belt_task"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/belt_task.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
